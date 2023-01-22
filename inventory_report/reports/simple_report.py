b251761669e4b06fcf2d12d567694975c2eb4ee9
Adolligit <adelson.onod@gmail.com>
2023-01-22 19:59:25 -0300

    feat: (SimpleReport) implementando método estático 'generate'

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,32 @@
+from datetime import datetime
+from collections import Counter
+
+
 class SimpleReport:
-    pass
+    @staticmethod
+    def generate(list_product: list) -> str:
+        oldest_manufacture = [
+            key["data_de_fabricacao"]
+            for key in list_product
+        ]
+
+        close_to_expiration = [
+            key["data_de_validade"]
+            for key in list_product
+            if key["data_de_validade"] > datetime.now()
+        ]
+      
+        enterprise = Counter(
+            key["nome_da_empresa"]
+            for key in list_product
+        ).most_common()[0][0]
+
+        return (
+            f"Data de fabricação mais antiga: {min(oldest_manufacture)}\n"
+            f"Data de validade mais próxima: {min(close_to_expiration)}\n"
+            f"Empresa com mais produtos: {enterprise}"
+        )
+
+# Data de fabricação mais antiga: YYYY-MM-DD
+# Data de validade mais próxima: YYYY-MM-DD
+# Empresa com mais produtos: NOME DA EMPRESA
