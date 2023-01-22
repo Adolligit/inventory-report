84e1e5f9323f6826992bf34f7e60dd0645497dc8
Adolligit <adelson.onod@gmail.com>
2023-01-22 18:01:25 -0300

    test: (Product) implementando teste de instanciação

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,15 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    pass
+    product = Product(
+        3,
+        'ProdutoTest1',
+        'Trybe',
+        '06/08/2000',
+        '-/-/-',
+        '3',
+        'Cuida bem do meu nenê.')
+
+    assert product.id == 1
