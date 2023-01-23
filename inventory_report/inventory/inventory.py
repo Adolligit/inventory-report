7d70c7df68ad0ceb5f543a10da3f8ed675564fde
Adolligit <adelson.onod@gmail.com>
2023-01-23 20:11:24 -0300

    feat/refactor: (Invetory)[__import_json_file] criando nova função para leitura de arquivos json; deixando funções privadas

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,4 +1,5 @@
 import csv
+import json
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
 
@@ -7,16 +8,21 @@ class Inventory:
     @staticmethod
     def import_data(path: str, type: str):
         if "csv" in path:
-            return Inventory.import_csv_file(path, type)
+            return Inventory.__import_csv_file(path, type)
+        elif "json" in path:
+            return Inventory.__import_json_file(path, type)
 
-    @staticmethod
-    def import_csv_file(path: str, type: str):
+    def __import_csv_file(path: str, type: str):
         with open(path, encoding="utf-8") as file:
             result = list(csv.DictReader(file, delimiter=',', quotechar='"'))
-            return Inventory.whats_the_type(type, result)
+            return Inventory.__whats_the_type(type, result)
 
-    @staticmethod
-    def whats_the_type(type: str, content: list):
+    def __import_json_file(path: str, type: str):
+        with open(path, encoding="utf-8") as file:
+            result = json.load(file)
+            return Inventory.__whats_the_type(type, result)
+
+    def __whats_the_type(type: str, content: list):
         return (
             CompleteReport.generate(content)
             if type == 'completo'
