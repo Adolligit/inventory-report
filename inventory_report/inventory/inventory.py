2cb008224ad52c4777b851794342d9909018fede
Adolligit <adelson.onod@gmail.com>
2023-01-23 19:53:21 -0300

    fix: (Inventory) [import_csv_file] adicionando parâmetro 'self' na função e dando tipos aos outros

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -5,18 +5,18 @@ from inventory_report.reports.simple_report import SimpleReport
 
 class Inventory:
     @classmethod
-    def import_data(self, path, type):
+    def import_data(self, path: str, type: str):
         if "csv" in path:
             return Inventory.import_csv_file(path, type)
 
     @classmethod
-    def import_csv_file(path, type):
+    def import_csv_file(self, path: str, type: str):
         with open(path, encoding="utf-8") as file:
             result = list(csv.DictReader(file, delimiter=',', quotechar='"'))
             return Inventory.whats_the_type(type, result)
 
     @classmethod
-    def whats_the_type(type, content):
+    def whats_the_type(self, type: str, content: list):
         return (
             CompleteReport.generate(content)
             if type == 'completo'
