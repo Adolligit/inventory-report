cc4e1c3d9314856f98666947ce2a9c4911f62114
Adolligit <adelson.onod@gmail.com>
2023-01-22 23:15:50 -0300

    feat: (CompleteReport) herdando função 'generate' da 'SimpleReport' e criando novos recursos

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,43 @@
-class CompleteReport:
-    pass
+from datetime import datetime
+from collections import Counter
+from inventory_report.reports.simple_report import SimpleReport
+
+
+class CompleteReport(SimpleReport):
+    @staticmethod
+    def generate(list_product: list) -> str:
+        oldest_manufacture = [
+            key["data_de_fabricacao"]
+            for key in list_product
+        ]
+
+        close_to_expiration = [
+            key["data_de_validade"]
+            for key in list_product
+            if key["data_de_validade"] > datetime.now().strftime("%Y-%m-%d")
+        ]
+
+        enterprise = Counter(
+            key["nome_da_empresa"]
+            for key in list_product
+        ).most_common()
+
+        stocked_products = ''
+
+        for company, qnt in enterprise:
+            stocked_products += (f"- {company}: {qnt}\n")
+
+        return (
+            f"Data de fabricação mais antiga: {min(oldest_manufacture)}\n"
+            f"Data de validade mais próxima: {min(close_to_expiration)}\n"
+            f"Empresa com mais produtos: {enterprise[0][0]}"
+            f"Produtos estocados por empresa:${stocked_products}"
+        )
+
+# Data de fabricação mais antiga: YYYY-MM-DD
+# Data de validade mais próxima: YYYY-MM-DD
+# Empresa com mais produtos: NOME DA EMPRESA
+# Produtos estocados por empresa:
+# - Physicians Total Care, Inc.: QUANTIDADE
+# - Newton Laboratories, Inc.: QUANTIDADE
+# - Forces of Nature: QUANTIDADE
