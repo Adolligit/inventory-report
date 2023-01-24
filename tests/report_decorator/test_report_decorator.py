396ef897cd0798041734559d3f28e10d417472e7
Adolligit <adelson.onod@gmail.com>
2023-01-24 19:35:01 -0300

    feat/test: (test_report_decorator) teste finalizado

--- tests/report_decorator/test_report_decorator.py
+++ tests/report_decorator/test_report_decorator.py
@@ -16,12 +16,14 @@ def test_decorar_relatorio():
     colored_report = ColoredReport(SimpleReport)
     colored_print = colored_report.generate([PRODUCT])
 
-    print("\033[36mAzul\033[0m \033[32mVerde\033[0m \033[31mVermelho\033[0m")
+    manufature_date = "Data de fabricação mais antiga:"
+    expiration_date = "Data de validade mais próxima:"
+    enterprise = "Empresa com mais produtos:"
 
     expected = (
-        "\033[32mData de fabricação mais antiga:\033[0m \033[36m2021-02-18\033[0m\n"
-        "\033[32mData de validade mais próxima:\033[0m \033[36m2023-09-17\033[0m\n"
-        "\033[32mEmpresa com mais produtos:\033[0m \033[31mTarget Corporation\033[0m"
+        f"\033[32m{manufature_date}\033[0m \033[36m2021-02-18\033[0m\n"
+        f"\033[32m{expiration_date}\033[0m \033[36m2023-09-17\033[0m\n"
+        f"\033[32m{enterprise}\033[0m \033[31mTarget Corporation\033[0m"
     )
 
     assert colored_print == expected
