c4d8c37a90ff468628326e166b2097e0b2a0afe8
Adolligit <adelson.onod@gmail.com>
2023-01-24 01:21:28 -0300

    feat/test: (test_product_report) o teste para a classe 'Product' foi implementado com sucesso

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        6,
+        "PS7",
+        "Sony",
+        "20-08-2030",
+        "20-08-2060",
+        "SCJLTDAPS7200830",
+        "Lugar mais seguro da sua casa...um cofre talvez?",
+    )
+
+    assert repr(product) == (
+        f"O produto {product.nome_do_produto}"
+        f" fabricado em {product.data_de_fabricacao}"
+        f" por {product.nome_da_empresa} com validade"
+        f" até {product.data_de_validade}"
+        f" precisa ser armazenado {product.instrucoes_de_armazenamento}."
+    )
