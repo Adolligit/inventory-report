eda986f810bbeaec5b98b049cb8b03b33effeec7
Adolligit <adelson.onod@gmail.com>
2023-01-25 01:59:51 -0300

    feat: (inventory_refactor) a classe foi implementada e testada com sucesso

--- inventory_report/inventory/inventory_refactor.py
+++ inventory_report/inventory/inventory_refactor.py
@@ -1,6 +1,7 @@
 from inventory_report.importer.importer import Importer
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.inventory.inventory_iterator import InventoryIterator
 
 
 class InventoryRefactor:
@@ -8,12 +9,15 @@ class InventoryRefactor:
         self.importer = importer
         self.data = []
 
+    def __iter__(self):
+        return InventoryIterator(self.data)
+
     def import_data(self, path: str, type: str):
         content = self.importer().import_data(path)
         self.data += content
         return self.__whats_the_type(type, content)
 
-    def __whats_the_type(type: str, content: list):
+    def __whats_the_type(self, type: str, content: list):
         return (
             CompleteReport.generate(content)
             if type == 'completo'
