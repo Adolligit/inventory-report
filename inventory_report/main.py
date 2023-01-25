0ad8c66b3c4f2feffd6eeacad108238930386e77
Adolligit <adelson.onod@gmail.com>
2023-01-25 02:18:11 -0300

    chore: (main) criando classe

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,9 @@
+import sys
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
+from inventory_report.inventory.inventory_refactor import InventoryRefactor
+
 def main():
-    pass
+    if len(sys.argv) < 3:
+        return print("Verifique os argumentos", file=sys.stderr)
