7101f7c8b6ecb08bca358908d6f3c9235bafc71c
Adolligit <adelson.onod@gmail.com>
2023-01-25 02:43:21 -0300

    feat: (main) a classe foi devidamente implementada e testada

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -4,6 +4,19 @@ from inventory_report.importer.json_importer import JsonImporter
 from inventory_report.importer.xml_importer import XmlImporter
 from inventory_report.inventory.inventory_refactor import InventoryRefactor
 
+
 def main():
     if len(sys.argv) < 3:
         return print("Verifique os argumentos", file=sys.stderr)
+
+    importers = {
+        "json": JsonImporter,
+        "csv": CsvImporter,
+        "xml": XmlImporter,
+    }
+
+    _, path, type = sys.argv
+    extension = path.split(".")
+    inventory_refactor = InventoryRefactor(importers[extension[-1]])
+    printed_result = inventory_refactor.import_data(path, type)
+    sys.stdout.write(printed_result)
