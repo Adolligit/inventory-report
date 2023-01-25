17d9fbb42e0966c1b65fdae2a866911bac775c68
Adolligit <adelson.onod@gmail.com>
2023-01-25 01:18:04 -0300

    chore: (inventory_refactor) iniciando criação de classe

---
+++ inventory_report/inventory/inventory_refactor.py
@@ -0,0 +1,21 @@
+from inventory_report.importer.importer import Importer
+from inventory_report.reports.complete_report import CompleteReport
+from inventory_report.reports.simple_report import SimpleReport
+
+
+class InventoryRefactor:
+    def __init__(self, importer: Importer):
+        self.importer = importer
+        self.data = []
+
+    def import_data(self, path: str, type: str):
+        content = self.importer().import_data(path)
+        self.data += content
+        return self.__whats_the_type(type, content)
+
+    def __whats_the_type(type: str, content: list):
+        return (
+            CompleteReport.generate(content)
+            if type == 'completo'
+            else SimpleReport.generate(content)
+        )
