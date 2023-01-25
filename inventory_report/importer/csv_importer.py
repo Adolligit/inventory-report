f00ecb5091c95ea8c9f182fed2ed8abbc355a70d
Adolligit <adelson.onod@gmail.com>
2023-01-25 01:05:40 -0300

    chore: indentando o código

--- inventory_report/importer/csv_importer.py
+++ inventory_report/importer/csv_importer.py
@@ -7,8 +7,6 @@ class CsvImporter(Importer):
     def import_data(path):
         if 'csv' in path:
             with open(path, encoding='utf-8') as file:
-                return list(
-                    csv.DictReader(file, delimiter=',', quotechar='"')
-                )
+                return list(csv.DictReader(file, delimiter=',', quotechar='"'))
         else:
             raise ValueError('Arquivo inválido')
