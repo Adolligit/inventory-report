5b69530b259a0a88c9d15139f61cbeff2aec5451
Adolligit <adelson.onod@gmail.com>
2023-01-25 01:37:08 -0300

    feat: (inventory_iterator) implementação da classe finalizada

--- inventory_report/inventory/inventory_iterator.py
+++ inventory_report/inventory/inventory_iterator.py
@@ -1,4 +1,12 @@
 from collections.abc import Iterator
 
+
 class InventoryIterator(Iterator):
-    
+    def __init__(self, data):
+        self.data = data
+        self.__index = 0
+
+    def __next__(self):
+        result = self.iterable[self.__index]
+        self.__index += 1
+        return result
